ba6a13efa41668ada6f8955604bff2c97008c4f8
Aditia Prasetian <aditiapras.dev@gmail.com>
2023-07-15 20:38:09 +0700

    delete unnecessary folder
    delete unnecessary folder

--- app/api/thesportsdb/[leagueid]/route.js
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-
-export async function GET(request, { params }) {
-  const res = await fetch(
-    `https://www.thesportsdb.com/api/v1/json/${process.env.NEXT_PUBLIC_API_KEY_DB}/lookupleague.php?id=${params.leagueid}`
-  ).then((r) => r.json());
-
-  return NextResponse.json(res);
-}
--- app/api/thesportsdb/route.js
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-
-export async function GET(request) {
-  const res = await fetch(
-    `https://www.thesportsdb.com/api/v1/json/${process.env.NEXT_PUBLIC_API_KEY_DB}/search_all_leagues.php?s=Soccer`
-  ).then((r) => r.json());
-
-  return NextResponse.json(res);
-}
