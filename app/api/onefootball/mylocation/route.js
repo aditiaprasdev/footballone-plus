e71317d9266c89588a40af8c4879de667c8e4940
Aditia Prasetian <aditiapras.dev@gmail.com>
2023-07-20 21:09:11 +0700

    update location api

--- app/api/onefootball/mylocation/route.js
+++ app/api/onefootball/mylocation/route.js
@@ -1,9 +1,6 @@
 import { NextResponse } from "next/server";
 
 export async function GET(request) {
-  // const res = await fetch(`https://www.fotmob.com/api/mylocation`, {
-  //   next: { revalidate: 1 },
-  // }).then((r) => r.json());
   const res = await fetch(
     `https://api.ipgeolocation.io/ipgeo?apiKey=${process.env.NEXT_PUBLIC_DEF_LOCATION}`
   ).then((r) => r.json());
