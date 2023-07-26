124e8d75f006c65b25c14787bc76d154c5f1d25b
Aditia Prasetian <aditiapras.dev@gmail.com>
2023-07-26 22:11:09 +0700

    update API_KEY name

--- app/api/onefootball/mylocation/route.js
+++ app/api/onefootball/mylocation/route.js
@@ -4,7 +4,7 @@ export async function GET(request) {
   const url = new URL(request.url);
 
   const res = await fetch(
-    `https://api.ipgeolocation.io/ipgeo${url.search}&apiKey=${process.env.NEXT_PUBLIC_DEF_LOCATION}`
+    `https://api.ipgeolocation.io/ipgeo${url.search}&apiKey=${process.env.NEXT_PUBLIC_API_KEY_LOCATION}`
   ).then((r) => r.json());
 
   if (res.status == 404) {
